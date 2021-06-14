a0f5f0e9e5a887a4e9c07afe17910d68786ad982
Yorman Hernández <yormanh@gmail.com>
2021-06-14 19:59:54 +0100

    Controladores en la escena. Haciendo prefabs y añadiendolo a las manos.

--- Assets/_MyAssets/_Scripts/HandCurrent.cs
+++ Assets/_MyAssets/_Scripts/HandCurrent.cs
@@ -6,6 +6,13 @@ using UnityEngine.XR;
 
 public class HandCurrent : MonoBehaviour
 {
+    [SerializeField] List<GameObject> mlControllerPrefabs;
+    [SerializeField] InputDeviceCharacteristics mInputDeviceCharacteristics;
+
+    private InputDevice mTargetDevice;
+
+    private GameObject mSpawnController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +22,28 @@ public class HandCurrent : MonoBehaviour
     private void Init()
     {
         List<InputDevice> lDevices = new List<InputDevice>();
-        InputDevices.GetDevices(lDevices);
+        //InputDevices.GetDevices(lDevices);
+        InputDevices.GetDevicesWithCharacteristics(mInputDeviceCharacteristics, lDevices);
 
         foreach (var lItem in lDevices)
         {
             Debug.Log(lItem.name + " -- " + lItem.characteristics);
+        }
+
+        if (lDevices.Count > 0)
+        {
+            mTargetDevice = lDevices[0];
+
+            Debug.Log("mTargetDevice: " + mTargetDevice.name);
+            GameObject lPrefab = mlControllerPrefabs.Find(c => c.name.Contains(mTargetDevice.name));
+
+            if (lPrefab)
+                mSpawnController = Instantiate(lPrefab, transform);
+            else
+            {
+                Debug.Log("No hemos encontrado el contralor");
+                mSpawnController = Instantiate(mlControllerPrefabs[0], transform);
+            }
         }
 
     }
@@ -28,6 +52,16 @@ public class HandCurrent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Init();
+        if (!mTargetDevice.isValid)
+        {
+            Init();
+            return;
+        }
+
+        mTargetDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool lTriggerButtonValue);
+
+        if (lTriggerButtonValue)
+            Debug.Log("lTriggerButtonValue presionado");
+
     }
 }
